e4e2696ce11bdd54d126936842f9222e7cbec8d8
JangaSK <91690211+JangaSK@users.noreply.github.com>
2021-09-30 22:34:12 +0530

    Add files via upload
    Assignments1, Assignments2, and Assignments3 are completed

---
+++ SDET_BATCH3_SUNILJANGA/SDET_Broadridge3/src/Assignment2/CharArrayToString.java
@@ -0,0 +1,15 @@
+package Assignment2;
+
+public class CharArrayToString {
+
+	public static void main(String[] args) {
+		
+		
+		char[] ch = {'D', 'C', 'O', 'E', ' ', 'Q', 'A'};
+	      String str = new String(ch);
+	      System.out.println(str);
+	     
+
+	}
+
+}
